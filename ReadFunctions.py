d21058024b434878e75166943734ba02ce2c94b5
Craig Bath <38102255+Delicious-Demon@users.noreply.github.com>
2020-07-02 22:29:30 -0400

    Update ReadFunctions.py

--- ReadFunctions.py
+++ ReadFunctions.py
@@ -1,7 +1,22 @@
+#Dependencies
+import pandas as pd
+
 #Parent of all Python reader functions
 
-#TODO - load csv or xlsx
+#Literal string conversion for file paths
+def raw_string(filepath):
+    cleaned=r'{}'.format(filepath)
+    return cleaned
 
+#Load csv or xlsx
+def read_data(filepath):
+    data=pd.read_csv(filepath)
+    print("File loaded with {} row and {} columns.".format(*data.shape))
+    return data
+    #TODO - conditional based on file type
+
+#res=read_data(r"C:\Users\cbath\Documents\Test Files\BMC_Post-Pipeline-ZSO-US_062220.csv")
+raw_string("C:\Users\cbath\Documents\Test Files\BMC_Post-Pipeline-ZSO-US_062220.csv")
 #TODO - convert to a data array
 
 
