255bba934e519f6f0c70e967a20151281db57ee7
Craig Bath <38102255+Delicious-Demon@users.noreply.github.com>
2020-07-06 14:51:21 -0400

    Update Read.py

--- Read.py
+++ Read.py
@@ -7,18 +7,19 @@ import re
 
 #Load csv or xlsx
 def read_data(filepath):
-    #TODO - make neater and just pick function here
-    m = re.match('Craig*','CraigBa',re.IGNORECASE)
+    #Try and be smart and handle all data types
+    m = re.search('.xlsx',filepath)
     if m:
-        dataframe = pd.read_csv(filepath)
-    else:
         dataframe = pd.read_excel(filepath)
+    else:
+        dataframe = pd.read_csv(filepath)
     print("File loaded with {} row and {} columns.".format(*dataframe.shape))
     print(dataframe.head(5))
     return dataframe
     #TODO - conditional based on file type
     
 #Example - always have to unicode escape for windows file paths using r''
-x=r'C:\Users\cbath\Documents\Test Files\BMC_Post-Pipeline-ZSO-US_062220.csv'
+x=r"C:\Users\cbath\Documents\Test Files\BMC_Post-Pipeline-ZSO-US_062220.csv"
+#x=r"C:\Users\cbath\Documents\Test Files\BMC_Post-Pipeline-ZSO-US_062220.xlsx"
 data=read_data(x)
 
